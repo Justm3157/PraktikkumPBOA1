0367a4cbb097fda7ac610bf18a6afb05e29cc0b8
Justm3157 <johanreinhartcalvin@gmail.com>
2026-03-19 16:24:25 +0700

    Latihan prak5

---
+++ Pertemuan5Latihan/src/Pajak.java
@@ -0,0 +1,12 @@
+/*  Pajak.java
+Deskripsi :berisi atribut dan method dalam class Pajak
+Pembuat  :Johan Reinhart Calvin
+Tanggal   :19/03/2026
+*/
+
+
+
+public interface Pajak {
+    //Mengembalikan hitungan pajak dari objek yang dipanggil
+    public double hitungPajak();
+}
